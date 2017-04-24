a6674210e921024a1bf8c035b719ed785de8dc76
wincev <info@winc-ev.org>
2017-04-24 13:40:08 +0200

    Added FrcFamily

---
+++ core/src/main/java/com/coinomi/core/coins/families/FrcFamily.java
@@ -0,0 +1,12 @@
+package com.coinomi.core.coins.families;
+
+/**
+ * @author Hendrik Richter
+ *
+ * This family contains Freicoin and Solidar.
+ */
+public abstract class FrcFamily extends BitFamily {
+    {
+        family = Families.FREICOIN;
+    }
+}
