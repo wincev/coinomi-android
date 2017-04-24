87a80f492aa0d0799f7f9fa7f92205fb6286444c
wincev <info@winc-ev.org>
2017-04-24 13:50:45 +0200

    Add Freicoin ID

--- wallet/src/main/java/com/coinomi/wallet/Constants.java
+++ wallet/src/main/java/com/coinomi/wallet/Constants.java
@@ -51,6 +51,7 @@ import com.coinomi.core.network.CoinAddress;
 import com.coinomi.stratumj.ServerAddress;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.coinomi.core.coins.FreicoinMain;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -214,7 +215,9 @@ public class Constants {
             new CoinAddress(RichcoinMain.get(),     new ServerAddress("richx-cce-1.coinomi.net", 5046),
                                                     new ServerAddress("richx-cce-2.coinomi.net", 5046)),
             new CoinAddress(IxcoinMain.get(),       new ServerAddress("ixc-cce-1.coinomi.net", 5047),
-                                                    new ServerAddress("ixc-cce-2.coinomi.net", 5047))
+                                                    new ServerAddress("ixc-cce-2.coinomi.net", 5047)),
+            new CoinAddress(FreicoinMain.get(),     new ServerAddress("electrum.freicoin.info", 50001))
+                                                    new ServerAddress("verify.ddns.net", 5001))
     );
 
     public static final HashMap<CoinType, Integer> COINS_ICONS;
@@ -264,6 +267,7 @@ public class Constants {
         COINS_ICONS.put(CoinID.CLUBCOIN_MAIN.getCoinType(), R.drawable.clubcoin);
         COINS_ICONS.put(CoinID.RICHCOIN_MAIN.getCoinType(), R.drawable.richcoin);
         COINS_ICONS.put(CoinID.IXCOIN_MAIN.getCoinType(), R.drawable.ixcoin);
+        COINS_ICONS.put(CoinID.FREICOIN_MAIN.getCoinType(), R.drawable.freicoin);
 
         COINS_BLOCK_EXPLORERS = new HashMap<CoinType, String>();
         COINS_BLOCK_EXPLORERS.put(CoinID.BITCOIN_MAIN.getCoinType(), "https://blockchain.info/tx/%s");
@@ -306,6 +310,7 @@ public class Constants {
         COINS_BLOCK_EXPLORERS.put(CoinID.EGULDEN_MAIN.getCoinType(), "https://chainz.cryptoid.info/efl/tx.dws?%s");
         COINS_BLOCK_EXPLORERS.put(CoinID.RICHCOIN_MAIN.getCoinType(), "https://explorer.richcoin.us/transaction?transaction=%s");
         COINS_BLOCK_EXPLORERS.put(CoinID.IXCOIN_MAIN.getCoinType(), "https://chainz.cryptoid.info/ixc/tx.dws?%s");
+        COINS_BLOCK_EXPLORERS.put(CoinID.FREICOIN_MAIN.getCoinType(), "http://freicoin.info/tx/%s");
     }
 
     public static final CoinType DEFAULT_COIN = BitcoinMain.get();
@@ -333,6 +338,7 @@ public class Constants {
             DogecoinMain.get(),
             EguldenMain.get(),
             FeathercoinMain.get(),
+            FreicoinMain.get(),
             GcrMain.get(),
             GuldenMain.get(),
             IxcoinMain.get(),
